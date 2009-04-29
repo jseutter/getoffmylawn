ce8d0c0a6b0a5d62a2cb7ce3537d3712b4b9684b
Wes Devauld <wes@devauld.ca>
2009-04-29 12:48:35 -0600

    Ninjas and SVG 1.1

--- lib/characters/enemies.py
+++ lib/characters/enemies.py
@@ -7,8 +7,8 @@ from base import Character, create_svg
 class Bunny(Character):
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
-        self.right = create_svg('crosshair.svg')
-        self.left = create_svg('crosshair.svg')
+        self.right = create_svg('ninja.svg')
+        self.left = create_svg('ninja.svg')
 #         self.dead = None 
 
 # A list of enemies
