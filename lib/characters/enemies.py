e4617fd84470fa58184cf90f6dfd9df663081e17
Ryan Cheung <rcheung@ryan-cheungs-inspiron-910.local>
2009-05-01 23:28:54 -0600

    Added GraffitiArtist to enemies. Loaded spraypaint sound in game.py, it is not yet used.

--- lib/characters/enemies.py
+++ lib/characters/enemies.py
@@ -41,7 +41,6 @@ class Ninja(Character):
         return 0
 
 
-# A list of enemies
 class Robot(Character):
     total_killed = 0
     in_a_row = 0
@@ -79,4 +78,37 @@ class Robot(Character):
             return 1
         return 0
 
-ENEMIES = [Ninja,Robot ]
+class GraffitiArtist(Character):
+    total_killed = 0
+
+    def __init__(self, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self.right = create_svg('graffiti_right.svg')
+        self.left = create_svg('graffiti_left.svg')
+        self.dead = create_svg('graffiti_death.svg')
+
+    def hit(self, x, y):
+        """
+        Return 1 if the target is hit by a bullet at x,y
+        """
+        # target is anchord center, bottom
+        width = self.right.width * self.scale
+        height = self.right.height * self.scale
+
+        # lower left hand coords
+        lx = self.x - (width)/2.0
+        ly = self.y
+
+        # upper right hand coords
+        tx = self.x + (width)/2.0
+        ty = self.y + height
+
+        if (x > lx and x < tx and
+            y > ly and y < ty):
+            GraffitiArtist.total_killed += 1
+            return 1
+        return 0
+
+# A list of enemies
+ENEMIES = [Ninja,Robot,GraffitiArtist,]
+
